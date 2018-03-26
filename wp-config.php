963b2487fd14fffefc47342b739718a2d9e7357f
Claude Janssen <claudejanssen.pro@gmail.com>
2018-03-26 15:02:46 +0200

    ignore

--- wp-config.php
+++ wp-config.php
@@ -77,7 +77,8 @@ $table_prefix  = 'wp_';
  *
  * @link https://codex.wordpress.org/Debugging_in_WordPress
  */
-define('WP_DEBUG', false);
+define('WP_DEBUG', true);
+define('WP_DEBUG_LOG', true);
 
 /* That's all, stop editing! Happy blogging. */
 
@@ -87,3 +88,5 @@ if ( !defined('ABSPATH') )
 
 /** Sets up WordPress vars and included files. */
 require_once(ABSPATH . 'wp-settings.php');
+//Plugins
+define('FS_METHOD', 'direct');
